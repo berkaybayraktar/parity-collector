a98aaeaa6c8dac2a02d0d0f9d4e29381a1c5c96e
Berkay Bayraktar <berkay.bayraktar@sistas.com.tr>
2018-12-06 13:55:00 +0300

    WS protokolü eklendi.

--- src/main/java/com/ub/paritycollector/config/WebSocketConfiguration.java
+++ src/main/java/com/ub/paritycollector/config/WebSocketConfiguration.java
@@ -1,24 +1,21 @@
 package com.ub.paritycollector.config;
 
+import com.ub.paritycollector.service.websocket.WebSocketMessageHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
+@EnableWebSocket
+public class WebSocketConfiguration implements WebSocketConfigurer {
+
+    @Autowired
+    private WebSocketMessageHandler webSocketMessageHandler;
 
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
-        config.setApplicationDestinationPrefixes("/app");
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(webSocketMessageHandler, "/");
     }
-
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/parity-websocket").withSockJS();
-    }
-
 }
